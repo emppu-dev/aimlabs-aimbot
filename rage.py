e1bbebf5499b731f500515d45e4b085c4a0fab07
emppu <83163481+emppu-dev@users.noreply.github.com>
2023-10-27 00:14:06 +0300

    Changed main file name to rage

---
+++ rage.py
@@ -0,0 +1,57 @@
+import win32api
+import win32con
+import time, datetime
+import keyboard
+from PIL import Image, ImageGrab
+from pynput.mouse import Button, Controller
+
+def log(text):
+    timestamp = datetime.datetime.utcfromtimestamp(time.time()).strftime("%H:%M:%S")
+    print(f"[{timestamp}] {text}")
+
+def click_and_move(x, y, color, mouse):
+    if color == (0, 250, 0):
+        tx, ty = x - 940, y - 560
+        log(f"{tx}, {ty}")
+        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, tx, ty, 0, 0)
+        time.sleep(0.007)
+        mouse.click(Button.left)
+        time.sleep(0.007)
+        #log(f"Restarting postion ({txx}, {tyx})")
+        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, -tx, -ty, 0, 0)
+        time.sleep(0.0013)
+
+
+def main():
+    print("Press Q to stop the program")
+    col = [(0, 250, 0)]
+    x, y = 500, 150
+    mouse = Controller()
+    num = 2
+    image = ImageGrab.grab()
+    while True:
+        color = image.getpixel((x, y))
+        click_and_move(x, y, color, mouse)
+        
+        if keyboard.is_pressed("q"):
+            print("You stopped the program")
+            break
+        
+        if x > 1200:
+            num += 1
+            if num % 2 == 0:
+                x = 513
+            elif num % 2 == 0:
+                x = 556
+            else:
+                x = 587
+            col = image.getpixel((320, 250))
+            image = ImageGrab.grab()
+        
+        y += 100
+        if y > 950:
+            x += 30
+            y = 150
+
+if __name__ == "__main__":
+    main()
